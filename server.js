629fde738a92237b187a2f2f98ebc9d33da856e5
Louie Divinagracia <divinagracialouie9@gmail.com>
2025-01-22 15:45:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 // Configure CORS to allow requests from your frontend's origin
 app.use(cors({
-  origin: ["http://193.203.162.228", "http://localhost:3000"], // Allow multiple origins
+  origin: ["*"], // Allow multiple origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
